8aaa733c988aa6fe495cdeb58346e19ae4cc0325
Marchelo <marchelo.bm@gmail.com>
2026-02-17 03:20:15 -0300

    v5.9 Se agrego xDBSample1
    para buscar y mostrar imagenes

---
+++ xDBSample1/src/xdbsample1/XDBSample1.java
@@ -0,0 +1,11 @@
+package xdbsample1;
+/**
+ * @web http://www.jc-mouse.net
+ * @author Mouse
+ */
+public class XDBSample1 {
+    
+    public static void main(String[] args) {
+        new interfaz().setVisible(true);
+    }
+}
---
+++ xDBSample1/src/xdbsample1/xBusqueda.java
@@ -0,0 +1,50 @@
+package xdbsample1;
+import java.io.File;
+import java.io.FileFilter;
+import javax.swing.DefaultListModel;
+/**
+ * @web http://www.jc-mouse.net
+ * @author Mouse
+ */
+public class xBusqueda {
+ 
+     /** Ruta donde se encuentran las imagenes */
+    //
+    private String appPath = System.getProperties().getProperty("user.dir");
+    public File directorio = new File( appPath + "\\imagenes\\" );    
+    //
+    //public File directorio = new File( "e:\\imagenes\\" );    
+    
+    /** Si la extensión es .JPG retorna TRUE, caso contrario FALSE */
+    private FileFilter filefilter = new FileFilter() {        
+        @Override
+        public boolean accept(File file) {           
+            return file.getName().endsWith(".jpg");
+        }
+    };
+    
+    /**
+     * Busca las imagenes que se encuentran en un directorio
+     * @param parametro String que corresponde al nombre del archivo a buscar
+     * @return DefaultListModel
+     */
+    public DefaultListModel buscar( String parametro )
+    {
+        DefaultListModel defaultListModel = new DefaultListModel ();
+        // Si es un directorio valido
+        if ( directorio.isDirectory() ) {			
+            // obtenemos su contenido
+            File[] ficheros = directorio.listFiles( filefilter );             
+            //y lo llenamos en un DefaultListModel
+            for ( File fichero : ficheros ) 
+            {
+                if( fichero.getName().toUpperCase().indexOf( parametro.toUpperCase() ) >= 0 )
+                {                        
+                    defaultListModel.addElement( fichero.getName() );
+                }                  
+            }
+        }        
+        return defaultListModel;
+    }
+    
+}//-->fin clase
